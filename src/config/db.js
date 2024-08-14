1161f2dd5c49f03f7482aa3dc4ad624ed3cb2687
Betoe <devrenjbr@gmail.com>
2024-08-14 10:51:44 -0300

    chore: Connecting to db
    Co-authored-by: Ulysses Oliveira <ulysses-lorde@users.noreply.github.com>

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const { client } = require('pg');
+
+const client = new Client({
+  connectionString: 'process.env.DATABASE_URL'
+});
+
+client.connect()
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Connection error', err.stack))
+
+module.exports = { client };
