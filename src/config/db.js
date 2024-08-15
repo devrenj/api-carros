db471398efa763f18b232bf958ff51ed6c79a7fd
Betoe <devrenjbr@gmail.com>
2024-08-15 08:47:42 -0300

    chore: Add db conection

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
-const { client } = require('pg');
+require('dotenv').config();
+const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: 'process.env.DATABASE_URL'
+  connectionString: process.env.DATABASE_URL
 });
 
+// Conectar e manter a conexão aberta
 client.connect()
-    .then(() => console.log('Connected to the database'))
-    .catch(err => console.error('Connection error', err.stack))
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
 
-module.exports = { client };
+module.exports = client;
